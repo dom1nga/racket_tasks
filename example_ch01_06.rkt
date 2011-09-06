c0b5de70352c21f529398d00dab13ab2d6fd4c06
dom1nga <beloved.dom1nga@gmail.com>
2011-09-07 01:03:45 +0300

    added example 6

---
+++ example_ch01_06.rkt
@@ -0,0 +1,26 @@
+#lang racket
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (square x)
+  (* x x))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+; программа будет бесконечно долго стремиться находить идеальное решение, т.к. если не срабатывает следствие всегда будет выполняться "иначе".
